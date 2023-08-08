03de93b672f6dd8bdc6e5876644ac138f2c8075f
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-08-08 09:33:04 +0800

    add styles

--- src/js/components/rowlist.js
+++ src/js/components/rowlist.js
@@ -5,56 +5,67 @@ export default function RowList(DOM){
     DOM.innerHTML = (`
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-30.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-40.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-30.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='gtx-16.jpeg' alt='rtx 30' />
+            <p>GTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-30.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-30.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-40.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.rtx30}' src='rtx-40.jpeg' alt='rtx 30' />
+            <p>RTX Series</p>
             <button type='button' class='${styles.button}'>Learn more</button>
         </div>
         <div class='${styles.row}'>
-            <h2>Introducing NVIDIA Jetson AGX Orin Industrial </h2>
-            <p>
-                Embedded edge AI is transforming industrial environments by introducing intelligence
-                and real-time processing to even the most challenging settings. Edge AI is increasingly 
-                being used in agriculture, construction, energy, aerospace, satellites, the public sector, 
-                and more. With the NVIDIA Jetson edge AI and robotics platform, you can deploy AI and compute
-                for sensor fusion in these complex environments.
-                At COMPUTEX 2023, NVIDIA announced the new Jetson AGX Orin Industrial module,
-                which brings the next level of computing to harsh environments. This new module
-                extends the capabilities of the previous-generation NVIDIA Jetson AGX Xavier
-                Industrial and the commercial Jetson AGX Orin modules, by bringing server-class
-                performance to ruggedized systems.
-            </p>
-            <p> <i class="fa-solid fa-angle-down ${styles.angle}"></i> Quick Links</p>
+            <div class='${styles.content}'>
+                <h2>Introducing NVIDIA Jetson AGX Orin Industrial </h2>
+                <p>
+                    Embedded edge AI is transforming industrial environments by introducing intelligence
+                    and real-time processing to even the most challenging settings. Edge AI is increasingly 
+                    being used in agriculture, construction, energy, aerospace, satellites, the public sector, 
+                    and more. With the NVIDIA Jetson edge AI and robotics platform, you can deploy AI and compute
+                    for sensor fusion in these complex environments.
+                    At COMPUTEX 2023, NVIDIA announced the new Jetson AGX Orin Industrial module,
+                    which brings the next level of computing to harsh environments. This new module
+                    extends the capabilities of the previous-generation NVIDIA Jetson AGX Xavier
+                    Industrial and the commercial Jetson AGX Orin modules, by bringing server-class
+                    performance to ruggedized systems.
+                </p>
+                <p class='${styles.dropdown}'> <i class="fa-solid fa-angle-down ${styles.angle}"></i> Quick Links</p>
+            </div>
+            <img src='chipset.png' alt='chipset image' />
         </div>
         <div class='${styles.row} ${styles.box}'>
             <img class='${styles.game}' src='resident.png' alt='game logo' />
             <h2>Nvidia Games</h2>
-            <p>
+            <p class='${styles['games-content']}'>
                 NVIDIA RTX™ and NVIDIA Omniverse™ deliver the performance 
                 to help professionals, creators, developers, and students worldwide
                 enhance creative workflows and build, operate, and connect metaverse
