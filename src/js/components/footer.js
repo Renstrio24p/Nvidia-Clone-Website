87ba198f921864e9d2ad2b861a396e5ec4fec448
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-08-08 09:47:04 +0800

    add styles

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,9 +1,11 @@
+import styles from '../../sass/modules/app.module.scss'
 
 export default function Footer(DOM){
 
     DOM.innerHTML = (`
         <div>
-            <p>&copy; copyright 2023 | Waren Gador</p>
+            <p>&copy; copyright 2023 | Waren Gador</p> 
+            <img class='${styles.nvidia}' src='nvidia-footer.svg' alt='nvidia' />
         </div>
     `)
 }
