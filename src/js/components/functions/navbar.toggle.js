b21abb6f8f629e99f22b7187432d1f0d87092bd5
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-08-09 15:54:57 +0800

    add collapsable navbar

---
+++ src/js/components/functions/navbar.toggle.js
@@ -0,0 +1,15 @@
+import styles from '../../../sass/modules/app.module.scss';
+
+export const NavToggles = () => {
+    const NavToggle = document.getElementById('bar');
+    const ShowReg = document.getElementById('icons');
+    const CollapsibleSection = document.getElementById('collapsibleSection');
+
+    NavToggle.addEventListener('click', Toggle);
+
+    function Toggle() {
+        CollapsibleSection.classList.toggle(`${styles.show}`);
+        ShowReg.classList.toggle('fa-xmark')
+        
+    }
+}
--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,13 +1,22 @@
-import styles from '../../sass/modules/app.module.scss'
+import styles from '../../sass/modules/app.module.scss';
+import { NavToggles } from './functions/navbar.toggle';
 
 export default function Navbar(DOM) {
-
     DOM.innerHTML = (`
-        <div class='${styles['logo-container']}'>
-            <img class='${styles.logo}' src='nvidia.png' alt='nvidia logo' />
-            <h1>Gaming Store</h1>
-        </div>
-        <button type='button' class='${styles.button}'>Login / Register</button>
-    `)
+        <div class='${styles.top}'>
+            <!-- Logo and title -->
+            <div class='${styles['logo-container']}'>
+                <img class='${styles.logo}' src='nvidia.png' alt='nvidia logo' />
+                <h1>Gaming Store</h1>
+            </div>
+            <button type='button' id='bar' class='${styles.button} ${styles.bar}'><i id='icons' class='fa-solid fa-bars'></i></button>
 
+        </div>
+            <!-- Collapsible section -->
+        <div class='${styles.show}' id='collapsibleSection'>
+                <button type='button' id='reg' class='${styles.button} ${styles.reg}'>Login / Register</button>
+        </div>
+    `);
+
+    NavToggles();
 }
