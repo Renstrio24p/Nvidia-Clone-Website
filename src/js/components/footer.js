b963dfaddfd94ac26d936c61f47179c8e77499e7
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-08-09 16:55:23 +0800

    add files

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer(DOM) {
 	DOM.innerHTML = `
         <div>
             <img class='${styles.nvidia}' src='nvidia-footer.svg' alt='nvidia' />
-            <p>&copy; copyright 2023 | Waren Gador</p> 
+            <p>&copy; copyright 2023 | <b>Waren Gador<b></p> 
         </div>
     `;
 }
